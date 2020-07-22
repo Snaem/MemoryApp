354e882d3bc2738dac96b1fc46fd975bc31e7782
Clement <clement.cheradame@free.fr>
2020-07-22 22:45:17 +0200

    feat(login/signin page): end of login page, starting of signin page css

--- src/app/pages/main/main-routing.module.ts
+++ src/app/pages/main/main-routing.module.ts
@@ -2,8 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { MainComponent } from './main.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [{ path: '', component: MainComponent }];
+const routes: Routes = [
+  {
+    path: 'dashboard', component: DashboardComponent
+  }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
