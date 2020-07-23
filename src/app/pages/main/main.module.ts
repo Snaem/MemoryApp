0f8772286e9a4b3c0f751fd37a9f788f4bbb7db0
Clement <clement.cheradame@free.fr>
2020-07-23 13:32:16 +0200

    fix(all): refactor of app schema

--- src/app/pages/main/main.module.ts
+++ src/app/pages/main/main.module.ts
@@ -11,9 +11,9 @@ import { AddMusicFormComponent } from './add-music/add-music-form/add-music-form
 import { PlaylistComponent } from './playlist/playlist.component';
 import { CreatePlaylistComponent } from './create-playlist/create-playlist.component';
 import { DetailsPlaylistComponent } from './playlist/details-playlist/details-playlist.component';
-import { NavbarComponent } from './shared/navbar/navbar.component';
-import { VideoListComponent } from './shared/video-list/video-list.component';
-import { VideoCardComponent } from './shared/video-list/video-card/video-card.component';
+import { NavbarComponent } from './shared/component/navbar/navbar.component';
+import { VideoListComponent } from './shared/component/video-list/video-list.component';
+import { VideoCardComponent } from './shared/component/video-list/video-card/video-card.component';
 
 
 @NgModule({
