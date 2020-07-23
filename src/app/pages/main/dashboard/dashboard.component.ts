cd6298f7988a9faafa1b9264161ca39437cfb7a5
Clement <clement.cheradame@free.fr>
2020-07-23 11:37:26 +0200

    feat(dashboard): squeleton of dashboard, fix navbar

--- src/app/pages/main/dashboard/dashboard.component.ts
+++ src/app/pages/main/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../auth/shared/services/user.service';
 
 @Component({
   selector: 'mmy-dashboard',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
   }
