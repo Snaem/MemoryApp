c3d625eb755c49e74468039afb8a003a8039f9d5
Clement <clement.cheradame@free.fr>
2020-08-24 13:14:08 +0200

    fix(login): fixed an error wher euser wasn't retrive

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { UserService } from '../shared/services/user.service';
 import { UserLog } from '../../shared/class/userLog';
 import { Router } from '@angular/router';
+import { AuthUser } from '../../shared/class/authUser';
 
 @Component({
   selector: 'mmy-login',
@@ -17,8 +18,10 @@ export class LoginComponent implements OnInit {
 
   logUser(user: UserLog) {
     this.userService.login(user).subscribe(() => {
-      this.userService.getMe().subscribe();
-      this.route.navigateByUrl('/dashboard');
+      this.userService.getMe().subscribe(userLogged => {
+        this.userService.currentUser = new AuthUser(userLogged.id, userLogged.username, userLogged.musics);
+        this.route.navigateByUrl('/dashboard');
+      });
     });
   }
 }
