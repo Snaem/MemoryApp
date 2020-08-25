c14eedd0644fe6b150a5b3a57c33423bf91213cd
Clement <clement.cheradame@free.fr>
2020-08-25 16:18:13 +0200

    feat(musicotheque): created component, strated to have musics in it

--- src/app/pages/main/musicotheque/musicotheque.component.ts
+++ src/app/pages/main/musicotheque/musicotheque.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MusicService } from '../shared/services/music.service';
+import { Music } from '../../shared/class/music';
 
 @Component({
   selector: 'mmy-musicotheque',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MusicothequeComponent implements OnInit {
 
-  constructor() { }
+  allMusic: Music[];
+
+  constructor(private musicService: MusicService) { }
 
   ngOnInit(): void {
+    this.musicService.getAllMusic().subscribe(data => {
+      this.allMusic = data;
+      this.allMusic.sort((a, b) => {
+        const miliA = new Date(a.addedAt).getTime();
+        const miliB = new Date(b.addedAt).getTime();
+        return miliA - miliB;
+      });
+    });
   }
 
 }
