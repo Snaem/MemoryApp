b4e5fd7b84f0a87ee6c108d5bdb375fa7019243b
Clement <clement.cheradame@free.fr>
2020-08-26 14:18:07 +0200

    feat(all): hide create playlist until it's finished, added error message when pwd or username is wrong

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -3,6 +3,7 @@ import { UserService } from '../shared/services/user.service';
 import { UserLog } from '../../shared/class/userLog';
 import { Router } from '@angular/router';
 import { AuthUser } from '../../shared/class/authUser';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'mmy-login',
@@ -11,7 +12,7 @@ import { AuthUser } from '../../shared/class/authUser';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private userService: UserService, private route: Router) { }
+  constructor(private userService: UserService, private route: Router, private matSnack: MatSnackBar) { }
 
   ngOnInit(): void {
   }
@@ -21,6 +22,10 @@ export class LoginComponent implements OnInit {
       this.userService.getMe().subscribe(userLogged => {
         this.userService.currentUser = new AuthUser(userLogged.id, userLogged.username, userLogged.musics);
         this.route.navigateByUrl('/acceuil');
+      });
+    }, (error) => {
+      this.matSnack.open('⚠ Le pseudo ou le mot de passe n\'est pas bon, veuillez réessayer', null, {
+        duration: 5000,
       });
     });
   }
